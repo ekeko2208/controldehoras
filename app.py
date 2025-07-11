8c7f5f461f8e07e458d424f56510ce2dac587423
ekeko2208 <arlesir2208@gmail.com>
2025-07-11 18:55:46 +0200

    modificacion de app.py a 512 hash

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     services = db.relationship('Service', backref='author', lazy=True) # One-to-many relationship with Service
 
     def set_password(self, password):
